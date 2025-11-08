7e69a684820a705ebf7945d10d5993c49327a976
@0xMR8anem <104733093+MR8anem@users.noreply.github.com>
2025-11-08 14:45:21 +0100

    Modify run_bandit.sh to use /bin/sh
    Updated script to use /bin/sh instead of /bin/bash and added echo statement for clarity.

--- scripts/run_bandit.sh
+++ scripts/run_bandit.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
-set -e
+#!/bin/sh
 
 TARGET_DIR="$1"
 REPORT_DIR="../reports/bandit"
 
+echo "Running Bandit scan on $TARGET_DIR..."
+
 mkdir -p "$REPORT_DIR"
 
-echo "Running Bandit scan..."
+# Run Bandit JSON and HTML reports
 ../.venv/bin/bandit -r "$TARGET_DIR" -f json -o "$REPORT_DIR/bandit.json"
 ../.venv/bin/bandit -r "$TARGET_DIR" -f html -o "$REPORT_DIR/bandit.html"
 
